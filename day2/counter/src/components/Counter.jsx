3f4adee343887b262ff788bc718519db419c7a4b
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-17 21:23:52 +0530

    final update

--- day2/counter/src/components/Counter.jsx
+++ day2/counter/src/components/Counter.jsx
@@ -4,7 +4,7 @@ const Counter = () => {
     const [count,setCount]=useState(0);
   return (
     <div>
-        <h1>Counter: {count}</h1>
+        <h1 className={count%2===0 ? "green":"red"}>Counter: {count}</h1>
         <div className='btn-grp'>
         <button className='btn' onClick={()=>setCount(count+1)}>Increment</button> 
         <button className='btn' onClick={()=> {if(count>0){setCount(count-1)}}}>Decrement</button> 
