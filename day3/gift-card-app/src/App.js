0ea9448a995eb00846e2d280c103782a1328427f
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-22 13:32:31 +0530

    update

---
+++ day3/gift-card-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card  />
+    </div>
+  );
+}
+
+export default App;
