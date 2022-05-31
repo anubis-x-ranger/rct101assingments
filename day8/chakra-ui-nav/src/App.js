97b82d07aa98b1af486632b5104faa5b03e5a51e
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-31 10:34:55 +0530

    update basic

--- day8/chakra-ui-nav/src/App.js
+++ day8/chakra-ui-nav/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Box, Button, ButtonGroup, Flex, Heading, Spacer, Stack, StackDivider, VStack } from '@chakra-ui/react'
 function App() {
+  const callStack=()=>{
+    return(
+      
+      <VStack  divider={<StackDivider borderColor='gray.200' />}  spacing={4}  align='stretch'>
+        <Box h='40px' bg='yellow.200'>
+          1
+        </Box>
+        <Box h='40px' bg='tomato'>
+          2
+        </Box>
+      </VStack>
+      
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex minWidth='max-content' minHeight='maxContent' alignItems='center' gap='2' justifyContent="space-between">
+  <Box p='2'>
+    <Heading size='md'>Logo</Heading>
+  </Box>
+  {/* <Spacer /> */}
+  <ButtonGroup gap='2'>
+  
+  <Button colorScheme='gray' size='sm' variant='link' onClick={()=>callStack()}>Inspiration</Button>
+  <Button colorScheme='gray' size='sm' variant='link'>Find Work</Button>
+    <Button colorScheme='gray' size='sm' variant='link'>Learn Design</Button>
+    <Button colorScheme='gray' size='sm' variant='link'>Hire Designers</Button>
+  </ButtonGroup>
+  <Spacer/>
+  <ButtonGroup gap='2'>
+    <Button colorScheme='gray' size='sm' variant='link'>Sign In</Button>
+    <Button colorScheme='pink' size='sm' variant='ghost'>Sign Up</Button>
+  </ButtonGroup>
+</Flex>
   );
 }
 
